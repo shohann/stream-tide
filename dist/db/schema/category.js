d46ba03c11b4a6921c21872a6c26bef492f2a672
Shohanur Rahman <shohanurr490@gmail.com>
2024-07-05 13:05:59 +0600

    Modified structure

--- dist/db/schema/category.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pg_core_1 = require("drizzle-orm/pg-core");
-const category = (0, pg_core_1.pgTable)("category", {
-    id: (0, pg_core_1.serial)("id").primaryKey(),
-    name: (0, pg_core_1.varchar)("name", { length: 255 }).notNull(),
-});
-exports.default = category;
--- dist/db/schema/profile.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const pg_core_1 = require("drizzle-orm/pg-core");
-const profile = (0, pg_core_1.pgTable)("profiles", {
-    id: (0, pg_core_1.serial)("id").primaryKey(),
-    state: (0, pg_core_1.varchar)("state", { length: 255 }).notNull()
-});
-exports.default = profile;

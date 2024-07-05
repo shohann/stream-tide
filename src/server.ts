2f331b778dd3da6cf3a0ce266fc8dc34ba83604a
Shohanur Rahman <shohanurr490@gmail.com>
2024-07-05 13:06:56 +0600

    Modified structure

--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import { errorHandler } from './libraries/error-handling';
 import { corsMiddleware, customHeadersMiddleware} from './middlewares/cors-middleware';
 import { requestLogger } from './middlewares/request-logger';
 
+
 let connection: Server;
 
 const createExpressApp = (): Application => {

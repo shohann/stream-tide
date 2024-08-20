b66cfc18315e26a964a53013ef4469f652adc37c
Shohanur Rahman <shohanurr490@gmail.com>
2024-08-20 17:03:21 +0600

    add logger in config

--- src/services/queue-service/queue-initializer.ts
+++ src/services/queue-service/queue-initializer.ts
@@ -1,6 +1,7 @@
 import path from "path";
 import { setupAllQueueEvents } from "./worker";
 import { createUploadDirs } from "../../server";
+import http from "http";
 
 const uploadDir = path.join(__dirname, "../../../uploads");
 
@@ -8,6 +9,14 @@ const setup = async () => {
   createUploadDirs(uploadDir);
   const status = setupAllQueueEvents();
   console.log("setupAllQueueEvents: ", status);
+
+  ///
+  const server = http.createServer();
+
+  const port = 3000; // You can change this port if needed
+  server.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
 };
 
 setup();

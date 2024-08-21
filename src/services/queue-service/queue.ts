47a2528a2f97157dfba9902283b95e8513a8d40d
Shohanur Rahman <shohanurr490@gmail.com>
2024-08-21 12:46:29 +0600

    updated config

--- src/services/queue-service/queue.ts
+++ src/services/queue-service/queue.ts
@@ -19,7 +19,7 @@ const queues = Object.values(QUEUE_EVENTS).map((queueName: string) => {
 
 export const addQueueItem = async (queueName: string, item: any) => {
   const queue = queues.find((q) => q.name === queueName);
-  console.log(`QUEUE: ${queue?.queueObj}`);
+  console.log(`QUEUE: ${queue?.name}`);
   if (!queue) {
     throw new Error(`Queue ${queueName} not found`);
   }
